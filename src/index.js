e3f35a911d6e3c3b64e0ca6c4e7c614c5c101d72
Railson25 <railsonsantiago123@gmail.com>
2020-04-22 22:01:06 -0300

    Configurando Redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { combineReducers, createStore } from 'redux'
+import { Provider } from 'react-redux'
 
 import Field from './ex/field'
 
+//reducers são funções puras sem efeito colateral 
+const reducers = combineReducers({
+    field: () => ({ value: 'Opa' })
+})
+
 ReactDOM.render(
-    <Field initialValue='Teste' />
+    //chamndo o método createStore com os metodos reducers
+    <Provider store={createStore(reducers)}>
+        <Field initialValue='Teste' />
+    </Provider>
 , document.getElementById('root'))    
