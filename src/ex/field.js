0050b9b5d0748ff0826d59efb0b8a066065a5a7a
Railson25 <railsonsantiago123@gmail.com>
2020-04-23 12:34:18 -0300

    Integrando o React-Redux (Parte 2)

--- src/ex/field.js
+++ src/ex/field.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { changeValue } from './fieldActions'
 
 
 class Field extends Component {
@@ -7,23 +9,20 @@ class Field extends Component {
         return (
             <div>
                 <label>{this.props.value}</label><br />
-                <input onChange={this.handleChange} value={this.props.value} />
+                <input onChange={this.props.changeValue} value={this.props.value} />
             </div>
         )
     } 
 }
 
-//mapeando o state para as props dos componentes 
-//controlando o state atraves do props
-//cria-se forada classe para que não haja dependencia do redux no componente
 function mapStateToProps(state) {
     return {
         value: state.field.value
     }
 }
 
-export default connect(mapStateToProps) (Field)
-//Padrao de Projeto conhecido decoration
-//está passando um componente retornando o mesmo componente
-//agora com o state mapeado pro valores do state global
-// da aplicaçao
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({ changeValue}, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps) (Field)
